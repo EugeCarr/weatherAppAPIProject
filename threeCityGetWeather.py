036c3361a8db2a13fabe83d50313f45005392d4a
EugeCarr <eugecarr@gmail.com>
2023-07-29 12:53:22 +0100

    Made it into a function

--- threeCityGetWeather.py
+++ threeCityGetWeather.py
@@ -2,6 +2,7 @@
 from flask import Flask
 import urllib.request, json
 from decouple import config
+from typing import List
 
 threeCities = [
     ("London", "England", "GB"),
@@ -10,33 +11,40 @@ threeCities = [
 ]
 
 
-
-openweather_apiKey = config("openweathermap")
-geoCodingBaseURL = "http://api.openweathermap.org/geo/1.0/direct?q="
-
-city_coords = {}
-
-for city in threeCities:
-    getURL = geoCodingBaseURL + city[0] + "," + city[1] + "," +  city[2] + "&limit=1&appid=" + openweather_apiKey
-    response = urllib.request.urlopen(getURL)
-    data = response.read()
-    dictionary_result = json.loads(data)
-    # print(f'lattitude: {dictionary_result[0]["lat"]} longitude: {dictionary_result[0]["lon"]}')
-    city_coords[city[0]] = (dictionary_result[0]["lat"], dictionary_result[0]["lon"])
+def getWeatherOfThreeCities(cities) -> dict:
+    assert isinstance(cities, list), "Cities input must be a tuple"
+    assert len(cities) == 3, "You must input 3 Cities"
+    assert all([len(city) == 3 for city in cities]), "Each city tuple must be in form: (City_name, State_name, Country_code)"
+    assert all( [type(l) == str for city in cities for l in city]), "Each part of the city tuple must be a string"
 
 
-weatherHourlyForecastBaseUrl = "http://api.openweathermap.org/data/2.5/forecast?"
+    openweather_apiKey = config("openweathermap")
+    geoCodingBaseURL = "http://api.openweathermap.org/geo/1.0/direct?q="
 
-city_temps = {}
+    city_coords = {}
 
-for city in threeCities:
-    cityCoordinate = city_coords[city[0]]
-    getURL = weatherHourlyForecastBaseUrl + "lat=" + str(cityCoordinate[0]) + "&lon=" + str(cityCoordinate[1]) + "&appid=" + openweather_apiKey + "&cnt=8&units=metric"
-    response = urllib.request.urlopen(getURL)
-    data = response.read()
-    dictionary_result = json.loads(data)
-    city_temps[city[0]] = dictionary_result["list"][-1]["main"]["temp"]
+    for city in cities:
+        getURL = geoCodingBaseURL + city[0] + "," + city[1] + "," +  city[2] + "&limit=1&appid=" + openweather_apiKey
+        response = urllib.request.urlopen(getURL)
+        data = response.read()
+        dictionary_result = json.loads(data)
+        # print(f'lattitude: {dictionary_result[0]["lat"]} longitude: {dictionary_result[0]["lon"]}')
+        city_coords[city[0]] = (dictionary_result[0]["lat"], dictionary_result[0]["lon"])
 
-print("The temperature forecasts of the 3 cities tomorrow are:")
-print(str(city_temps))
 
+    weatherHourlyForecastBaseUrl = "http://api.openweathermap.org/data/2.5/forecast?"
+
+    city_temps = {}
+
+    for city in cities:
+        cityCoordinate = city_coords[city[0]]
+        getURL = weatherHourlyForecastBaseUrl + "lat=" + str(cityCoordinate[0]) + "&lon=" + str(cityCoordinate[1]) + "&appid=" + openweather_apiKey + "&cnt=8&units=metric"
+        response = urllib.request.urlopen(getURL)
+        data = response.read()
+        dictionary_result = json.loads(data)
+        city_temps[city[0]] = dictionary_result["list"][-1]["main"]["temp"]
+
+    return city_temps
+
+test = getWeatherOfThreeCities(cities=threeCities)
+print(str(test))
